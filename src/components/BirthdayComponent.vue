43477bf0e53ee07b693c5b5212a9b2ee04f48d37
Marc Pont <mpont91@gmail.com>
2025-04-01 11:21:19 +0200

    Add gift

--- src/components/BirthdayComponent.vue
+++ src/components/BirthdayComponent.vue
@@ -21,6 +21,21 @@
     >
       Encendre 🔥
     </button>
+    <button class="gift-interaction-button" @click="onGiftClick">
+      Regal 🎁
+    </button>
+    <div v-if="showGiftModal" class="modal">
+      <div class="modal-content">
+        <div class="modal-header">
+          <img src="dvisi.svg" alt="Logo" class="modal-logo" />
+          <span class="close" @click="showGiftModal = false">&times;</span>
+        </div>
+        <p>Per molts anys Anna!.</p>
+        <p>El teu regal es un dinar/sopar al restaurant Dvisi!</p>
+        <p>Menu a la carta :)</p>
+        <img src="cute.gif" />
+      </div>
+    </div>
     <canvas ref="canvasRef"></canvas>
   </div>
 </template>
@@ -38,6 +53,7 @@ const countdown = ref<number>(10)
 const canBlow = ref<boolean>(true)
 const isDisabled = ref<boolean>(false)
 const isSceneVisible = ref<boolean>(false)
+const showGiftModal = ref<boolean>(false)
 
 onMounted(async () => {
   if (canvasRef.value) {
@@ -85,6 +101,10 @@ const debounceClick = () => {
     isDisabled.value = false
   }, 500)
 }
+
+const onGiftClick = () => {
+  showGiftModal.value = true
+}
 </script>
 
 <style scoped>
@@ -117,6 +137,27 @@ const debounceClick = () => {
   transform: translateX(-50%) scale(1.05);
 }
 
+.gift-interaction-button {
+  position: absolute;
+  bottom: 2rem;
+  right: 2rem;
+  transform: translateX(-50%);
+  padding: 1rem 2rem;
+  font-size: 1.2rem;
+  background-color: gold;
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
+  font-weight: bold;
+  box-shadow: 0 0 20px rgba(255, 215, 0, 0.5);
+  transition: transform 0.2s ease;
+  z-index: 1;
+}
+
+.gift-interaction-button:hover {
+  transform: translateX(-50%) scale(1.05);
+}
+
 .cake-scene {
   position: absolute;
   top: 0;
@@ -129,5 +170,79 @@ const debounceClick = () => {
 
 .cake-scene.visible {
   opacity: 1;
+}
+
+.modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+}
+
+.modal-content {
+  background: linear-gradient(to bottom, #004d40, #000000);
+  padding: 2rem;
+  max-width: 90%;
+  width: 400px; /* Ancho fijo para el modal */
+  text-align: center;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.3); /* Sombra más suave */
+  position: relative;
+  transform: translateY(-20px); /* Animar entrada desde arriba */
+  opacity: 0;
+  animation:
+    fadeIn 0.3s ease-out forwards,
+    slideIn 0.3s ease-out forwards;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes slideIn {
+  from {
+    transform: translateY(-20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+.modal-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+
+.close {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  font-size: 2rem;
+  cursor: pointer;
+  color: #888;
+  transition: color 0.2s ease;
+}
+
+.close:hover {
+  color: #333;
+}
+
+.modal-content p {
+  color: white;
+  font-size: 1.1rem;
+  line-height: 1.5;
+  margin-bottom: 1.5rem;
 }
 </style>
